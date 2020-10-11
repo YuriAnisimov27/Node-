9434057b27fdd3148c07775e1f011f9c709cd646
YuriAnisimov27 <yurianisimov27@gmail.com>
2020-10-11 23:37:03 +0300

    init: create-react-app, Materialize

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const {Schema, model} = require('mongoose')
 const schema = new Schema({
   email: {type: String, required: true, unique: true},
   password: {type: String, required: true},
-  links: [{type: Types.ObjectId, ref: 'Link'}]
+  links: [{type: Schema.Types.ObjectID, ref: 'Link'}]
 })
 
 module.exports = model('User', schema)
