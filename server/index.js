ee0182695942f370eba7e56448cd632448d3b694
Yuri Anisimov <yurianisimov27@gmail.com>
2020-11-28 10:12:03 +0300

    feat: add user to db

--- server/index.js
+++ server/index.js
@@ -1,1 +1,24 @@
-console.log('hello server')
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('config')
+const authRouter = require('./routes/auth.routes')
+
+const app = express()
+const PORT = config.get('serverPort')
+
+app.use(express.json())
+app.use('/api/auth', authRouter)
+
+const start = async () => {
+  try {
+    await mongoose.connect(config.get('dbUrl'))
+
+    app.listen(PORT, () => {
+      console.log(`Server started on PORT: ${PORT}`)
+    })
+  } catch (e) {
+
+  }
+}
+
+start()
