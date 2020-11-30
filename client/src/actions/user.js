254f0f5e89ac2141b207390e1810342ffb1a056f
Yuri Anisimov <yurianisimov27@gmail.com>
2020-11-30 16:01:39 +0300

    feat: add autologin

--- client/src/actions/user.js
+++ client/src/actions/user.js
@@ -28,3 +28,21 @@ export const login = (email, password) => {
     }
   };
 };
+
+export const auth = (email, password) => {
+  return async dispatch => {
+    try {
+      const response = await axios.get(`http://localhost:5000/api/auth/auth`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`
+        }
+      });
+      dispatch(setUser(response.data.user));
+      console.log(response.data.user);
+      localStorage.setItem('token', response.data.token);
+    } catch (e) {
+      alert(e.response.data.message);
+      localStorage.removeItem('token')
+    }
+  };
+};
