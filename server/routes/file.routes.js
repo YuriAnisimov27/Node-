4a85c93d4fb8dce5c8281aa144ae9b673d2e1224
Yuri Anisimov <yurianisimov27@gmail.com>
2020-12-10 15:07:08 +0300

    init: rest-api app

--- server/routes/file.routes.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express')
-const authMiddleware = require('../middleware/auth.middleware')
-const fileController = require('../controllers/fileController')
-
-const router = new Router()
-
-router.post('', authMiddleware, fileController.createDir)
-
-module.exports = router;
