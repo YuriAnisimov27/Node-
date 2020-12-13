3f8ab25874661a7ce746365b0e62fde08c988961
YuriAnisimov27 <yurianisimov27@gmail.com>
2020-12-13 18:11:20 +0300

    feat: add Loader component

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
 
-const app = express()
+const app = express();
 
 const CONTACTS = [
   {id: 1, name: 'John', value: '975-99-92', marked: false}
-]
+];
 
 app.get('/api/contacts', (req, res) => {
-  res.status(200).json(CONTACTS)
-})
+  res.status(200).json(CONTACTS);
+});
 
-app.use(express.static(path.resolve(__dirname, 'client')))
+app.use(express.static(path.resolve(__dirname, 'client')));
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
-})
-app.listen(3000, () => console.log('Server has been started on port 3000...'))
+  res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
+});
+app.listen(3000, () => console.log('Server has been started on port 3000...'));
